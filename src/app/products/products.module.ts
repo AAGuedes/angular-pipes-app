e4bc1eb9ff385bc458cc7b8b1c414101215159ec
AAGuedes <airam.a.g.85@gmail.com>
2023-04-24 08:55:03 +0100

    UpperCase, LowerCase, TitleCase Pipes

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
@@ -16,7 +17,8 @@ import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.compo
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    PrimeNgModule,
+    ProductsRoutingModule,
   ]
 })
 export class ProductsModule { }
